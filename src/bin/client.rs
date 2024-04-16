748e9427c23bccc518c0b5e4f68099a7f2763c60
Thomas Rory Gummerson <thomas@gummerson.no>
2024-04-16 23:10:08 +0200

    client cleanup, part of server cleanup

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,8 @@
 use serde::Deserialize;
+use serde_json::Value;
 use std::io::{stdin, Result};
 use std::net::SocketAddr;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{stdout, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::task::JoinHandle;
 
@@ -11,45 +12,52 @@ struct Endpoint {
     request: serde_json::Value,
 }
 
+fn json_value_to_vec_of_u8(value: Value) -> Option<Vec<u8>> {
+    match value {
+        Value::String(s) => Some(s.into()),
+        Value::Array(arr) => {
+            let mut ret = Vec::new();
+            for val in arr {
+                if let Value::Number(n) = val {
+                    if let Some(x) = n.as_u64() {
+                        if x < 256 {
+                            ret.push(x as u8);
+                        } else {
+                            return None;
+                        }
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            Some(ret)
+        }
+        _ => None,
+    }
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config: Vec<Endpoint> = serde_json::from_reader(stdin())?;
-    let mut connections: Vec<JoinHandle<Result<()>>> = Vec::new();
+    let mut connections: Vec<JoinHandle<Result<()>>> = Vec::with_capacity(config.len());
     for endpoint in config {
         connections.push(tokio::spawn(async move {
             let mut conn = TcpStream::connect(endpoint.socket_address).await?;
-            let mut u: Vec<u8> = Vec::new();
-            let buf = match endpoint.request {
-                serde_json::Value::String(s) => {
-                    for c in s.as_bytes() {
-                        u.push(*c);
-                    }
-                    u.as_slice()
-                }
-                serde_json::Value::Array(v) => {
-                    for val in v {
-                        u.push(match val {
-                            serde_json::Value::Number(n) => match n.as_u64() {
-                                Some(x) => x as u8,
-                                None => panic!("Number doesn't fit into u64"),
-                            },
-                            _ => panic!("Invalid array value"),
-                        })
-                    }
-                    u.as_slice()
-                }
-                _ => panic!("Invalid request value"),
-            };
-            conn.write_all(buf).await?;
-            conn.shutdown().await?;
-            let mut s = String::new();
-            conn.read_to_string(&mut s).await?;
-            println!("{}", s);
+            if let Some(v) = json_value_to_vec_of_u8(endpoint.request) {
+                conn.write_all(v.as_slice()).await?;
+                conn.shutdown().await?;
+                let mut s = String::new();
+                conn.read_to_string(&mut s).await?;
+                s.push('\n');
+                stdout().write_all(s.as_bytes()).await?;
+            }
             Ok(())
         }))
     }
     for connection in connections {
-        tokio::try_join!(connection)?.0?;
+        connection.await??;
     }
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,14 +1,17 @@
-use tokio::io::BufWriter;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
 use std::collections::HashMap;
-use std::io::Result;
+use std::env::args;
+use std::io;
+use std::net::IpAddr;
 use std::net::SocketAddr;
+use std::num::ParseIntError;
 use std::ops::Sub;
+use std::str::FromStr;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
 use std::time::SystemTime;
+use tokio::io::BufWriter;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::task::JoinHandle;
 
 type State = HashMap<String, u64>;
@@ -21,79 +24,83 @@ type ListenerStatesHashMap = HashMap<SocketAddr, State>;
 type ListenerStatesMutex = Mutex<ListenerStatesHashMap>;
 type ListenerStates = Arc<ListenerStatesMutex>;
 
+fn create_listener_states() -> ListenerStates {
+    ListenerStates::new(ListenerStatesMutex::new(ListenerStatesHashMap::new()))
+}
+
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let args = std::env::args();
-    let listener_states =
-        ListenerStates::new(ListenerStatesMutex::new(ListenerStatesHashMap::new()));
+async fn main() -> io::Result<()> {
+    let args = args();
+    let listener_states = create_listener_states();
     let global_state = Arc::new(Mutex::new(GlobalState {
         changed: false,
         last_written: SystemTime::UNIX_EPOCH,
         state: State::new(),
     }));
     let global_state_file_writer = global_state.clone();
-    let mut listeners: Vec<JoinHandle<Result<()>>> = Vec::new();
+    let mut listeners: Vec<JoinHandle<io::Result<()>>> = Vec::new();
     for arg in args.skip(1) {
-        // TODO: allow passing only port
         let ls = listener_states.clone();
         let gs = global_state.clone();
-        let arg = arg.clone();
+        let port_result: Result<u16, ParseIntError> = arg.clone().parse();
+        if let Err(_) = port_result {
+            continue;
+        }
         listeners.push(tokio::spawn(async move {
-            let listener = tokio::net::TcpListener::bind(&arg).await?;
+            let addr =
+                SocketAddr::new(IpAddr::from_str("127.0.0.1").unwrap(), port_result.unwrap());
+            let listener = tokio::net::TcpListener::bind(addr).await?;
             loop {
                 let (mut socket, _) = listener.accept().await?;
                 let ls = ls.clone();
                 let gs = gs.clone();
-                let arg = arg.clone();
                 tokio::spawn(async move {
-                let (mut read_socket, write_socket) = socket.split();
-                let mut bw = BufWriter::new(write_socket);
-                let addr: SocketAddr = arg.parse().unwrap();
-                let mut string = String::new();
-                read_socket.read_to_string(&mut string).await.unwrap();
-                println!("{}", string);
-                let response = {
-                    let mut ls = ls.lock().unwrap();
-                    let mut gs = gs.lock().unwrap();
-                    let state = match ls.get_mut(&addr) {
-                        Some(map) => map,
-                        None => {
-                            match ls.insert(addr, HashMap::<String, u64>::new()) {
-                                Some(_) => unreachable!(),
-                                None => (),
-                            };
-                            match ls.get_mut(&addr) {
-                                Some(map) => map,
-                                None => unreachable!(),
+                    let (mut read_socket, write_socket) = socket.split();
+                    let mut bw = BufWriter::new(write_socket);
+                    let mut string = String::new();
+                    read_socket.read_to_string(&mut string).await.unwrap();
+                    let response = {
+                        let mut ls = ls.lock().unwrap();
+                        let mut gs = gs.lock().unwrap();
+                        let state = match ls.get_mut(&addr) {
+                            Some(map) => map,
+                            None => {
+                                match ls.insert(addr, HashMap::<String, u64>::new()) {
+                                    Some(_) => unreachable!(),
+                                    None => (),
+                                };
+                                match ls.get_mut(&addr) {
+                                    Some(map) => map,
+                                    None => unreachable!(),
+                                }
                             }
-                        }
-                    };
-                    let segments: Vec<&str> = string.split(' ').collect();
-                    let amount: u64 = match segments[0].parse() {
-                        Ok(s) => s,
-                        Err(_) => unreachable!(),
-                    };
-                    let animal: String = String::from(segments[1]);
-                    let val = match state.get(&animal) {
-                        Some(n) => *n,
-                        None => 0,
-                    };
-                    let global_val = match state.get(&animal) {
-                        Some(n) => *n,
-                        None => 0,
-                    };
-                    state.insert(animal.clone(), val + amount);
-                    gs.state.insert(animal, global_val + amount);
-                    gs.changed = true;
+                        };
+                        let segments: Vec<&str> = string.split(' ').collect();
+                        let amount: u64 = match segments[0].parse() {
+                            Ok(s) => s,
+                            Err(_) => unreachable!(),
+                        };
+                        let animal: String = String::from(segments[1]);
+                        let val = match state.get(&animal) {
+                            Some(n) => *n,
+                            None => 0,
+                        };
+                        let global_val = match gs.state.get(&animal) {
+                            Some(n) => *n,
+                            None => 0,
+                        };
+                        state.insert(animal.clone(), val + amount);
+                        gs.state.insert(animal, global_val + amount);
+                        gs.changed = true;
 
-                    let response: String = serde_json::to_string(state).unwrap();
-                    response
-                };
-                println!("{}", response);
-                bw.write_all(response.as_bytes()).await.unwrap();
-                bw.flush().await.unwrap();
-                socket.shutdown().await.unwrap();
-            });
+                        let response: String = serde_json::to_string(state).unwrap();
+                        response
+                    };
+                    println!("{}", response);
+                    bw.write_all(response.as_bytes()).await.unwrap();
+                    bw.flush().await.unwrap();
+                    socket.shutdown().await.unwrap();
+                });
             }
         }));
     }
